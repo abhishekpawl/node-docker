df2c0654b6c3c983e107f8e32c4096aeb5a806bd
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-01-16 01:23:22 +0530

    Set up mongo service

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+/* logic to handle state before mongo is initialized */
+const connectWithRetry = () => {
+  mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("Successfully connected to DB"))
+  .catch((err) => {
+    console.log(err)
+    setTimeout(connectWithRetry, 5000)
+  });
+};
+
+connectWithRetry();
 
 app.get('/', (req, res) => {
   res.json([
