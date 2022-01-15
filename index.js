0eaacacb46b4779c5cf5aab8f68f189f825fb88c
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-01-15 16:55:22 +0530

    Added initial files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      name: 'Abhishek Paul',
+      profile: 'Personal',
+      email: 'abhishek.paulcp.dbs@gmail.com'
+    }
+  ])
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port:${port}...`);
+})
